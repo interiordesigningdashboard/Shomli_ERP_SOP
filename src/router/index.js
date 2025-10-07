f0a56d52f7f341e8aa6e578b49adcb0560213fce
vk <vishnukarthik2912@gmail.com>
2025-10-07 19:25:31 +0530

    updated the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WhatIsSop from '@/pages/WhatIsSop.vue'
-import FactoryPurchaseSop from '@/pages/FactoryPurchaseSop.vue'
-import IBRSOP from '@/pages/IBRSOP.vue'
-import SitePurchaseSOP from '@/pages/SitePurchaseSOP.vue'
+import WhatIsSop from '/pages/WhatIsSop.vue'
+import FactoryPurchaseSop from '../pages/FactoryPurchaseSop.vue'
+import IBRSOP from '../pages/IBRSOP.vue'
+import SitePurchaseSOP from '../pages/SitePurchaseSOP.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
